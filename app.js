074d3c1b3f0d119086dee36a17587fe24238df4a
fiixed <fiixed@gmail.com>
2020-11-08 16:04:22 -0500

    express params

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-//jshint esversion:6
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const express = require('express');
+const ejs = require('ejs');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -13,50 +11,43 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 let posts = [];
 
-app.get("/", function(req, res) {
-  res.render("home" , {
+app.get('/', (req, res) => {
+  res.render('home' , {
     startingContent: homeStartingContent, 
     posts: posts
   });
-  
+ 
 });
 
-app.get("/about", function(req, res) {
-  res.render("about" , {startingContent: aboutContent});
+app.get('/about', (req, res) =>{
+  res.render('about' , {startingContent: aboutContent});
 });
 
-app.get("/contact", function(req, res) {
+app.get('/contact', (req, res) => {
   res.render("contact" , {startingContent: contactContent});
 });
 
-app.get("/compose", function(req, res) {
-  res.render("compose");
+app.get('/compose', (req, res) => {
+  res.render('compose');
 });
 
-app.post("/compose", function(req, res) {
+app.post('/compose', (req, res) => {
   let post = {
     title: req.body.postTitle,
     content: req.body.postBody
   };
   posts.push(post);
-  res.redirect("/");
+  res.redirect('/');
   
 });
 
-
-
-
-
-
-
-app.get("/posts/:postName", function(req, res) {
+app.get('/posts/:postName', (req, res) => {
   console.log(req.params.postName);
 });
-
 
 
 app.listen(3000, function() {
